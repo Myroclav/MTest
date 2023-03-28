a764baec38522ee915f95148670731ab34ba6294
Stosh <stosh.lv.ua@gmail.com>
2023-03-29 00:21:11 +0300

    fix next button, clean up code

--- app/src/main/java/com/example/mtest/screens/FirstFragment.kt
+++ app/src/main/java/com/example/mtest/screens/FirstFragment.kt
@@ -1,11 +1,12 @@
 package com.example.mtest.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.mtest.MAIN
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.mtest.R
 import com.example.mtest.databinding.FragmentFirstBinding
 
@@ -15,19 +16,25 @@ class FirstFragment : Fragment() {
     lateinit var binding: FragmentFirstBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentFirstBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnNext.setOnClickListener{
-            MAIN.navController.navigate(R.id.action_firstFragment_to_secondFragment)
+        Log.d(TAG, "onViewCreated ----> Start")
+
+        binding.btnNext.setOnClickListener {
+            Log.d(TAG, "onViewCreated ----> clicked!!!")
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
         }
     }
 
-
+    companion object {
+        val TAG = FirstFragment::class.simpleName
+    }
 }
